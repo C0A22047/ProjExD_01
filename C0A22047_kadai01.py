f422522453521a3b72002d67729e93a36d0350fd
c0a22047 <c0a220479f@edu.teu.ac.jp>
2023-09-26 15:24:45 +0900

    1

---
+++ C0A22047_kadai01.py
@@ -0,0 +1,9 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    clock  = pg.time.Clock()
+    bg_img1 = pg.image.load("ProjExD2023/ex01/fig/pg_bg.jpg")
+    
