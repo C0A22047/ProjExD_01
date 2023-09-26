fe2ae4b7206bbe518284fef02362bbd2f5212b2d
c0a22047 <c0a220479f@edu.teu.ac.jp>
2023-09-26 15:27:11 +0900

    2

--- C0A22047_kadai01.py
+++ C0A22047_kadai01.py
@@ -6,4 +6,5 @@ def main():
     screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img1 = pg.image.load("ProjExD2023/ex01/fig/pg_bg.jpg")
-    
+    bg_img2 = pg.image.load("ProjExD2023/ex01/fig/3.png")
+    flip_image = pg.transform.flip(bg_img2, True, False)
